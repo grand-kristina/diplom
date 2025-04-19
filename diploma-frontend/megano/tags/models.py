8cdc0d781ed8f8a267376c36cf649fe8aa357ac1
Roman <31464179+grand-roman@users.noreply.github.com>
2025-04-19 16:49:44 +0700

    fix

--- diploma-frontend/megano/tags/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-
-    class Meta:
-        verbose_name = "Тэг"
-        verbose_name_plural = "Тэги"
--- diploma-frontend/megano/tags/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import TagView
-
-app_name = "tags"
-
-urlpatterns = [
-    path("tags", TagView.as_view(), name="tags"),
-]
